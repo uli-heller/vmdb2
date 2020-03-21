8227c1d98a9ae550bba534f41d9e372796a65d56
Lars Wirzenius <liw@liw.fi>
2020-03-21 16:05:53 +0200

    Change: only produce PDF if pdflatex is available

--- format.sh
+++ format.sh
@@ -22,15 +22,18 @@ pandoc \
     -o vmdb2.html \
     "$tmp/prelude.mdwn" vmdb/plugins/*.mdwn
 
-pandoc \
-    --toc \
-    --number-sections \
-    -Vdocumentclass=report \
-    -Vgeometry:a4paper \
-    -Vfontsize:12pt \
-    -Vmainfont:FreeSerif \
-    -Vsansfont:FreeSans \
-    -Vmonofont:FreeMonoBold \
-    '-Vgeometry:top=2cm, bottom=2.5cm, left=2cm, right=1cm' \
-    -o vmdb2.pdf \
-    "$tmp/prelude.mdwn" vmdb/plugins/*.mdwn
+if command -v pdflatex > /dev/null
+then
+    pandoc \
+        --toc \
+        --number-sections \
+        -Vdocumentclass=report \
+        -Vgeometry:a4paper \
+        -Vfontsize:12pt \
+        -Vmainfont:FreeSerif \
+        -Vsansfont:FreeSans \
+        -Vmonofont:FreeMonoBold \
+        '-Vgeometry:top=2cm, bottom=2.5cm, left=2cm, right=1cm' \
+        -o vmdb2.pdf \
+        "$tmp/prelude.mdwn" vmdb/plugins/*.mdwn
+fi
