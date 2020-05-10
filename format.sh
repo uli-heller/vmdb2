f9fbf491c39a68c9dba9d55fe250f058773fadcd
Lars Wirzenius <liw@liw.fi>
2020-05-10 07:45:47 +0300

    Change: format using Subplot, if available

--- format.sh
+++ format.sh
@@ -4,36 +4,42 @@ set -eu
 
 cleanup()
 {
-    rm -rf "$tmp"
+    rm -f tmp.md
 }
 
-tmp="$(mktemp -d)"
 trap cleanup EXIT
 
-version="$(git describe)"
-sed "s/^date: .*/date: $version/" vmdb2.mdwn > "$tmp/prelude.mdwn"
-
-pandoc \
-    --self-contained \
-    --standalone \
-    --css vmdb2.css \
-    --toc \
-    --number-sections \
-    -o vmdb2.html \
-    "$tmp/prelude.mdwn" vmdb/plugins/*.mdwn
-
-if command -v pdflatex > /dev/null
+cat vmdb2.md vmdb/plugins/*.mdwn > tmp.md
+if command -v sp-docgen > /dev/null
 then
+    sp-docgen tmp.md -o vmdb2.html
+    if command -v pdflatex > /dev/null
+    then
+	sp-docgen tmp.md -o vmdb2.pdf
+    fi
+else
     pandoc \
-        --toc \
-        --number-sections \
-        -Vdocumentclass=report \
-        -Vgeometry:a4paper \
-        -Vfontsize:12pt \
-        -Vmainfont:FreeSerif \
-        -Vsansfont:FreeSans \
-        -Vmonofont:FreeMonoBold \
-        '-Vgeometry:top=2cm, bottom=2.5cm, left=2cm, right=1cm' \
-        -o vmdb2.pdf \
-        "$tmp/prelude.mdwn" vmdb/plugins/*.mdwn
+	--self-contained \
+	--standalone \
+	--css vmdb2.css \
+	--toc \
+	--number-sections \
+	-o vmdb2.html \
+	tmp.md
+
+    if command -v pdflatex > /dev/null
+    then
+	pandoc \
+            --toc \
+            --number-sections \
+            -Vdocumentclass=report \
+            -Vgeometry:a4paper \
+            -Vfontsize:12pt \
+            -Vmainfont:FreeSerif \
+            -Vsansfont:FreeSans \
+            -Vmonofont:FreeMonoBold \
+            '-Vgeometry:top=2cm, bottom=2.5cm, left=2cm, right=1cm' \
+            -o vmdb2.pdf \
+            tmp.md
+    fi
 fi
