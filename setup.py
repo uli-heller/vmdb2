4beb979f545bf130caa1d7d7532f3759aa9f6e96
Lars Wirzenius <liw@liw.fi>
2020-09-24 08:59:08 +0300

    fix(setup.py): drop manpage building

--- setup.py
+++ setup.py
@@ -15,55 +15,11 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from distutils.core import setup, Extension
-from distutils.cmd import Command
-from distutils.command.build import build
-from distutils.command.clean import clean
-import os
+from distutils.core import setup
 import glob
-import subprocess
 
 
 import vmdb
-
-
-class Build(build):
-    def run(self):
-        build.run(self)
-        self.build_manpage("vmdb2", "")
-        self.format_yarns()
-
-    def build_manpage(self, program, lang):
-        return
-        # building manpage fails on by unstable CI worker for mysterious
-        # reasons, will re-enable later
-        print("building manpage for %s (lang=%s)" % (program, lang))
-        self.generate_troff(program, lang)
-        self.format_man_as_txt(program)
-
-    def generate_troff(self, program, lang):
-        with open("%s.1%s" % (program, lang), "w") as f:
-            subprocess.check_call(
-                [
-                    "python3",
-                    program,
-                    "--generate-manpage=%s.1%s.in" % (program, lang),
-                    "--output=%s.1" % program,
-                ],
-                stdout=f,
-            )
-
-    def format_man_as_txt(self, program):
-        env = dict(os.environ)
-        env["MANWIDTH"] = "80"
-        with open("%s.1.txt" % program, "w") as f:
-            subprocess.check_call(
-                ["man", "-l", "%s.1" % program], ["col", "-b"], stdout=f, env=env
-            )
-
-    def format_yarns(self):
-        print("building yarns")
-        subprocess.check_call(["make", "-C", "yarns"])
 
 
 setup(
@@ -76,5 +32,4 @@ setup(
     scripts=["vmdb2"],
     packages=["vmdb", "vmdb.plugins"],
     data_files=[("share/man/man1", glob.glob("*.1"))],
-    cmdclass={"build": Build},
 )
